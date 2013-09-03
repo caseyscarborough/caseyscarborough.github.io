c7ea09d977be705bac5422d654f3783fabdff1b9
Casey Scarborough <caseyscarborough@gmail.com>
2013-09-03 19:09:33 -0400

    Remove text justify on mobile devices. Update main.js.

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,11 +1,3 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-43691924-1', 'caseyscarborough.com');
-ga('send', 'pageview');
-
 $(document).ready(function() {
   $('.content').fadeIn(400);
 
@@ -15,12 +7,18 @@ $(document).ready(function() {
     if(link.indexOf('/') === 0 && link !== document.URL) {
       event.preventDefault();  // Prevent the default action
       $('.content').fadeOut(400, redirectPage); // Fade out content and redirect
-    } else if(link === document.URL) {
-      event.preventDefault();
     }
   });
 
   function redirectPage() {
     window.location = link;
   }
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43691924-1', 'caseyscarborough.com');
+  ga('send', 'pageview');
 });
