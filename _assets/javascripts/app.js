9b24af9cdb8649927996ad688b4592e25449ca9b
Casey Scarborough <caseyscarborough@gmail.com>
2014-03-01 10:58:27 -0500

    Use CDN for highlight.js.

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,21 +1,4 @@
-#= require highlight
-
-$(document).ready(function() {
-  // $('.content').fadeIn(400);
-
-  // // Capture link click events
-  // $('a').click(function(event) {
-  //   link = this.href;
-  //   if(link.indexOf('/') === 0 && link !== document.URL) {
-  //     event.preventDefault();  // Prevent the default action
-  //     $('.content').fadeOut(400, redirectPage); // Fade out content and redirect
-  //   }
-  // });
-
-  function redirectPage() {
-    window.location = link;
-  }
-
+$(function() {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -23,4 +6,6 @@ $(document).ready(function() {
 
   ga('create', 'UA-43691924-1', 'caseyscarborough.com');
   ga('send', 'pageview');
+
+  hljs.initHighlightingOnLoad();
 });
