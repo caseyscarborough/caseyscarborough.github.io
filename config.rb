3f1cbea5d161ab71928e762626c42c9ab35d6f65
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-20 11:17:08 -0500

    Move images, javascripts, and stylesheets to assets folder.

--- config.rb
+++ config.rb
@@ -45,9 +45,9 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
 
 ignore '*.yml'
 
